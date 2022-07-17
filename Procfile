801e187d3158bd4036da55d949c41fc1c19962dc
KevinDGnanih <kevin.gnanih@gmail.com>
2022-07-17 22:18:03 +0000

    Deployment heroku part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn carter_music_co.wsgi
--- carter_music_co/settings.py
+++ carter_music_co/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
@@ -28,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['carter-music-co.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -76,11 +77,15 @@ WSGI_APPLICATION = 'carter_music_co.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
