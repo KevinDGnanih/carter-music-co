dae334a5a5682cb93ae345b75b0f40ce8428cbfa
KevinDGnanih <kevin.gnanih@gmail.com>
2022-08-14 22:25:55 +0000

    Added color option

--- products/views.py
+++ products/views.py
@@ -10,7 +10,7 @@ from .models import Product, Category, Brand
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
-    products = Product.objects.filter(color=None)
+    products = Product.objects.all()
     query = None
     categories = None
     brands = None
