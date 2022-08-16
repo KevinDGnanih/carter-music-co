3c343c2690985a4b3e05f08dbed6daa8bc671ac1
KevinDGnanih <kevin.gnanih@gmail.com>
2022-08-16 23:16:39 +0000

    Added checkout success logic

--- checkout/signals.py
+++ checkout/signals.py
@@ -13,7 +13,7 @@ def update_on_save(sender, intance, created, **kwargs):
 
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, intance, **kwargs):
+def update_on_delete(sender, intance, **kwargs):
     """
     Update order total on lineitem delete
     """
