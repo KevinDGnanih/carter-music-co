2f827e49a4ba9660ab1dc64510062e44a0ecddf0
KevinDGnanih <kevin.gnanih@gmail.com>
2022-08-16 20:21:16 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LAKuEE7PmGUuoBlgFK7JrtXwctnzVU89xzEhBMX64x8heprDhdaqgcl7M4faSPe59rYPeKMvfYIRu69GK1GQ8Vm00Fplyskwh',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
