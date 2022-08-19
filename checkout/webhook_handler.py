5298b909ec00622ee2410439525b7504ab35c9f4
KevinDGnanih <kevin.gnanih@gmail.com>
2022-08-19 17:43:37 +0000

    Working on the delay webhook handlers

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from .models import Order
 
 
 class StripeWH_Handler:
@@ -20,8 +21,75 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.secceeded webhook from Stripe
         """
+        intent = event.data.object
+        pid = intent.id
+        bag = intent.metadata.bag
+        save_info = intent.metadata.save_info
+
+        billing_details = intent.charges.data[0].billing_details
+        shipping_details = intent.shipping
+        grand_total = round(intent.data.charges[0].amount / 100, 2)
+
+        # Clean data in the shipping details
+        for field, value in shipping_details.address.item():
+            if value == "":
+                shipping_details.address[field] = None
+
+        order_exists = False
+        attempt = 1
+        while attempt <= 5
+            try:
+                order = Order.objects.get(
+                    full_name__iexact=shipping_details.name,
+                    phone_number__iexact=shipping_details.phone,
+                    country__iexact=shipping_details.country,
+                    postcode__iexact=shipping_details.postal_code,
+                    town_or_city__iexact=shipping_details.city,
+                    street_address1__iexact=shipping_details.line1,
+                    street_address2__iexact=shipping_details.line2,
+                    county__iexact=shipping_details.county,
+                    grand_total=grand_total,
+                )
+                order_exists = True
+                break
+            except Order.DoesNotExist:
+                attempt += 1
+                time.sleep(1)
+        if order_exists:
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]} | SUCCESS: verified order already in database',
+                status=200)
+        else:
+            order = None
+            try:
+                order =order.objects.create(
+                    full_name=shipping_details.name,
+                    phone_number=shipping_details.phone,
+                    country=shipping_details.country,
+                    postcode=shipping_details.postal_code,
+                    town_or_city=shipping_details.city,
+                    street_address1=shipping_details.line1,
+                    street_address2=shipping_details.line2,
+                    county=shipping_details.county,
+                    grand_total=grand_total,
+                )
+                product = Product.objects.get(id=item_id)
+                if isinstance(item_data, int):
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        product=product,
+                        quantity=item_data,
+                    )
+                    order_line_item.save()
+            except Exception as e:
+                if order:
+                    order.delete()
+                return HttpResponse(
+                    content=f'Webhook received: {event["type"]} | ERROR: {e}',
+                    status=500)
+
         return HttpResponse(
-            content=f'Webhook received: {event["type"]}',
+            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200
         )
 
