df742c48ef6f1e6d34cb002b85adacb48858fa3f
KevinDGnanih <kevin.gnanih@gmail.com>
2022-08-21 10:19:40 +0000

    Updated the settings file

--- carter_music_co/settings.py
+++ carter_music_co/settings.py
@@ -174,6 +174,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
