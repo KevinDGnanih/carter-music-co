78a3809e31794f0a469fb418ece34f6eec2e5259
KevinDGnanih <kevin.gnanih@gmail.com>
2022-09-23 20:16:22 +0000

    Added about page and app

--- carter_music_co/urls.py
+++ carter_music_co/urls.py
@@ -31,5 +31,6 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('about/', include('about.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = 'carter_music_co.views.handler404'
