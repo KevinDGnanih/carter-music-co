44429a4103157dfc14a5202e949f6c984e466948
KevinDGnanih <kevin.gnanih@gmail.com>
2022-09-27 20:01:28 +0000

    Made migration and migrate the wishlist

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class Category(models.Model):
@@ -46,6 +47,7 @@ class Product(models.Model):
         max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    users_wishlist = models.ManyToManyField(settings.AUTH_WISHLIST_MODEL, related_name="user_wishlist", blank=True)
 
     def __str__(self):
         return self.name
