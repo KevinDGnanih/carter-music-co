7fbfb0edf22a2db40a8a0d155cbe93987971506a
KevinDGnanih <kevin.gnanih@gmail.com>
2023-03-26 14:02:03 +0000

    latest version

--- carter_music_co/settings.py
+++ carter_music_co/settings.py
@@ -32,6 +32,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 ALLOWED_HOSTS = [
     'carter-music-co.onrender.com/',
     'carter-music-co-origin.vercel.app',
+    'carter-music-co.vercel.app',
     '127.0.0.1',
     '.vercel.app',
     'localhost']
